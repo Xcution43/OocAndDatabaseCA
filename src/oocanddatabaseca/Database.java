7cad916f513e7a238818e9bff7ec110fe97fda30
2021120 <146199356+2021120@users.noreply.github.com>
2023-12-11 15:55:51 +0000

    Creating Database.java
    Database abstract class created and comments are written

---
+++ src/oocanddatabaseca/Database.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oocanddatabaseca;
+
+/**
+ *
+ * @author temuu
+ */
+public abstract class Database {
+    
+    //We made it static because we do not need setupDB object
+    //It can run from this class
+    //Since it is abstract class we made them protected
+    
+    protected final static String DB_BASE_URL = "jdbc:msql://localhost";
+    protected final static String USER = "ooc2023"; 
+    protected final static String PASSWORD = "ooc2023";
+    protected final static String DB_NAME = "ooc_database_assessment";
+    protected final static String TABLE_NAME = "userData";
+    
+    //jdbc:msql://localhost/ooc_database_assessment  / forward slash connects base url to userdata 
+    
+    protected final static String DB_URL = DB_BASE_URL + "/" + DB_NAME;
+}
+    
+
--- src/oocanddatabaseca/OocAndDatabaseCA.java
+++ src/oocanddatabaseca/OocAndDatabaseCA.java
@@ -15,7 +15,7 @@ public class OocAndDatabaseCA {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
         
 
         //Testing how the user class and currentID works
